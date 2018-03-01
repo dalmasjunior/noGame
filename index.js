a7e2b4508a0d6d0df27c6a9a86730b953268ab78
Paulo Roberto Dalmas Júnior <dalmasjunior@gmail.com>
2018-03-01 01:57:01 -0300

    alteracoes feitas em casa

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
 
 const routes = require('./routes');
 const config = require('./config');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const router = new Router();
 
 const users = require('./users/user-router');
 
+
 router.use('/users', users);
 
 module.exports = router;
